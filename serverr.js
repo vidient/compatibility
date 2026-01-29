8f30bdf4d6c15b851ca9eb637a92843a4ecb67f4
v <swallowedson@outlook.com>
2026-01-29 20:55:57 +0100

    Add files via upload

--- serverr.js
+++ serverr.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "publicc")));
